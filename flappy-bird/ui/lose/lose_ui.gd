63ee6f9233c6ec8a2106d20e2854123199d66c8e
Hugo <hugo.reyhol@educa.jcyl.es>
2025-05-10 14:16:03 +0200

    Arreglado posicion botones LoseUI

--- flappy-bird/ui/lose/lose_ui.gd
+++ flappy-bird/ui/lose/lose_ui.gd
@@ -6,8 +6,17 @@ signal change_scene_requested(new_scene)
 
 const MENU_SCENE = "res://stages/start/start.tscn"
 
-@onready var anim_player: AnimationPlayer = $AnimationPlayer
-@onready var replay_button: TextureButton = $ReplayButton
+@export var anim_player: AnimationPlayer
+@export var replay_button: TextureButton
+@export var buttons_cont: VBoxContainer
+
+
+func _ready() -> void:
+	buttons_cont.position.x = (
+		get_viewport().get_visible_rect().size.x
+		- buttons_cont.size.x
+	) / 2.0
+	pass
 
 
 # Detecta cuando se activa el boton por teclado y reinicia
